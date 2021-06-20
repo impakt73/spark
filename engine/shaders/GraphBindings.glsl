3f67c276aec56cb4488acca367bea89bdc921a29
Gregory Mitrano <gregory.mitrano@gmail.com>
2021-06-20 01:00:22 -0400

    Enable Required Shader Features
    This change updates the low level vulkan code to enable all shader
    features required by the common shader code.
    
    Specifically, 64-bit atomics are now required/enabled.

--- engine/shaders/GraphBindings.glsl
+++ engine/shaders/GraphBindings.glsl
@@ -3,3 +3,8 @@ layout(std430, set = 1, binding = 1) restrict buffer Buffers
 {
     uint Data[];
 } uBuffers[64];
+
+layout(std430, set = 1, binding = 1) restrict buffer Buffers64
+{
+    uint64_t Data[];
+} uBuffers64[64];
--- vkutil/src/lib.rs
+++ vkutil/src/lib.rs
@@ -1,7 +1,10 @@
 use ash::{
     extensions::{ext, khr},
     version::{DeviceV1_0, EntryV1_0, InstanceV1_0},
-    vk,
+    vk::{
+        self, PhysicalDeviceFeatures2, PhysicalDeviceVulkan11Features,
+        PhysicalDeviceVulkan12Features,
+    },
 };
 use std::borrow::Cow;
 use std::ffi::{CStr, CString};
@@ -375,9 +378,29 @@ impl VkDevice {
                 })
                 .collect::<Vec<_>>();
 
+            let mut device_1_0_features = PhysicalDeviceFeatures2::default();
+            let mut device_1_1_features = PhysicalDeviceVulkan11Features::default();
+            let mut device_1_2_features = PhysicalDeviceVulkan12Features::default();
+
+            device_1_0_features.p_next =
+                &mut device_1_1_features as *mut PhysicalDeviceVulkan11Features as _;
+            device_1_1_features.p_next =
+                &mut device_1_2_features as *mut PhysicalDeviceVulkan12Features as _;
+
+            // Set up required shader features
+
+            device_1_0_features.features.shader_int16 = 1;
+            device_1_0_features.features.shader_int64 = 1;
+
+            device_1_1_features.storage_buffer16_bit_access = 1;
+
+            device_1_2_features.shader_buffer_int64_atomics = 1;
+            device_1_2_features.shader_shared_int64_atomics = 1;
+
             let device_create_info = vk::DeviceCreateInfo::builder()
                 .queue_create_infos(&queue_infos)
-                .enabled_extension_names(&device_extensions);
+                .enabled_extension_names(&device_extensions)
+                .push_next(&mut device_1_0_features);
 
             let device =
                 instance
