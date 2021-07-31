190f074fc40b858ba5f9f05078950d1d7cf3de42
Gregory Mitrano <gregory.mitrano@gmail.com>
2021-07-31 11:05:40 -0400

    Signed Buffer Aliases + Barrier Cleanup (#32)
    * Signed Buffer Aliases + Barrier Cleanup
    
    This change adds a new variant of the 64-bit shader storage buffer
    descriptors that's signed and also removes some redundant barriers.
    
    * Force vk-mem's ash to version 0.32.1
    
    This works around an incompatible update to the ash crate and allows CI
    to build properly.

--- engine/shaders/GraphBindings.glsl
+++ engine/shaders/GraphBindings.glsl
@@ -8,3 +8,8 @@ layout(std430, set = 1, binding = 1) restrict buffer Buffers64
 {
     uint64_t Data[];
 } uBuffers64[64];
+
+layout(std430, set = 1, binding = 1) restrict buffer SignedBuffers64
+{
+    int64_t Data[];
+} uSignedBuffers64[64];
